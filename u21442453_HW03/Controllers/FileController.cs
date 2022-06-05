c12b544d3233aa869f641e2926dd436691ed1afb
CanadianMapleFarmer <66121776+CanadianMapleFarmer@users.noreply.github.com>
2022-06-06 01:14:05 +0200

    stable version

---
+++ u21442453_HW03/Controllers/FileController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using u21442453_HW03.Models;
+
+namespace u21442453_HW03.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FilePage()
+        {
+            string rootPath = Server.MapPath("~/Media/Documents");
+
+            List<FileModel> fileArr = new List<FileModel>();
+
+            string fN = "";
+            string fP = "";
+
+            foreach (string file in Directory.EnumerateFiles(rootPath))
+            {
+                fP = file.Replace(@"\", @"/");
+                fN = fP.Split('/').Last();
+
+                System.Diagnostics.Debug.WriteLine($"filePath: {fP} \n fileName: {fN}"); 
+                fileArr.Add(new FileModel(fN, fP));
+            };
+
+            return View(fileArr);
+        }
+    }
+}
--- u21442453_HW03/Models/FileModel.cs
+++ u21442453_HW03/Models/FileModel.cs
@@ -7,5 +7,19 @@ namespace u21442453_HW03.Models
 {
     public class FileModel
     {
+        private string fileName = "";
+        private string fileType = "";
+        private string filePath = "";
+
+        public FileModel(string fileName, string filePath)
+        {
+            this.fileName = fileName;
+            this.filePath = filePath;
+            this.fileType = "." + fileName.Split('.').Last();
+        }
+
+        public string FileName { get => fileName; set => fileName = value; }
+        public string FileType { get => fileType; }
+        public string FilePath { get => filePath; set => filePath = value; }
     }
 }
